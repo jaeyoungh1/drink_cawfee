e7f7f50cac86ecece67bc3c0a4a707782984efce
Jae Hwang <jaeyoung1997@gmail.com>
2022-10-26 10:25:43 -0700

    styling navbar

--- react-app/src/components/HomePage/homePage.js
+++ react-app/src/components/HomePage/homePage.js
@@ -12,28 +12,29 @@ const HomePage = () => {
 
 
     return (
-        <div className='homepage-container'>
-            <div className='homepage-splash'>
-                <div className='homepage-text'>
-                    <div className='homepage-text-upgrade'>Easily Upgrade</div>
-                    <div className='homepage-text-upgrade'>Your Coffee</div>
-                    <div>The best coffee delivered fresh to your door from the top craft roasters in California</div>
-                    <div>
-                        {/* <div>Try Now</div> */}
-                        <div>Shop All Coffee</div>
-                    </div>
-                </div>
-                <img src="https://res.cloudinary.com/roastcollective/image/upload/f_auto/v1664460478/web/homepage/desktop-hero-revised.png" />
-            </div>
-            <div className='recent-activity-container'>
-                <h2 className='recent-activity-title'>
-                    Recent Activity
-                </h2>
-                <div className='recent-cards-container'>
-                </div>
-            </div>
+        <div></div>
+        // <div className='homepage-container'>
+        //     <div className='homepage-splash'>
+        //         <div className='homepage-text'>
+        //             <div className='homepage-text-upgrade'>Easily Upgrade</div>
+        //             <div className='homepage-text-upgrade'>Your Coffee</div>
+        //             <div className= 'homepage-text-best'>The best coffee delivered fresh to your door from the top craft roasters in California</div>
+        //             <div>
+        //                 <div>Try Now</div>
+        //                 <div>Shop All Coffee</div>
+        //             </div>
+        //         </div>
+        //         <img src="https://res.cloudinary.com/roastcollective/image/upload/f_auto/v1664460478/web/homepage/desktop-hero-revised.png" />
+        //     </div>
+        //     <div className='recent-activity-container'>
+        //         <h2 className='recent-activity-title'>
+        //             Recent Activity
+        //         </h2>
+        //         <div className='recent-cards-container'>
+        //         </div>
+        //     </div>
 
-        </div>
+        // </div>
     )
 }
 
--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,23 +1,80 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import LogoutButton from '../auth/LogoutButton';
 import menu from '../../icons/menu.svg'
 import cawfeeCrow from '../../icons/cawfee_crow.png'
+import profile from '../../icons/profile.svg'
 import './NavBar.css'
 
 const NavBar = () => {
+  const [visibility, setVisible] = useState(false)
+  const [coffeeVisible, setCoffeeVisible] = useState(false)
+
+  // let history = useHistory()
   return (
-    <nav className='homepage-navbar'>
-      <div>
+    <nav>
+      <div className='homepage-navbar'>
+        {/* <div>
         <img src={menu}/>
+      </div> */}
+        <div className='navbar-logo' >
+          <NavLink style={{ textDecoration: 'none', color: 'black' }} to='/'>Cawfee</NavLink>
+          {/* Cawfee <img height='100' width='100' src={cawfeeCrow}/> */}
+        </div>
+
+        <div
+          onMouseEnter={() => {
+            setVisible(true)
+            setCoffeeVisible(true)
+          }}
+          onMouseLeave={() => setVisible(false)}
+        >
+          <NavLink className='navbar-link' style={{ textDecoration: 'none', color: 'black' }}
+            to='/'>Shop</NavLink>
+        </div>
+        <div
+          onMouseEnter={() => {
+            setVisible(true)
+            setCoffeeVisible(false)
+          }}
+          onMouseLeave={() => setVisible(false)}>
+          <NavLink className='navbar-link' style={{ textDecoration: 'none', color: 'black' }}
+            to='/'>Roasters</NavLink></div>
+        <div
+          onMouseEnter={() => setVisible(true)}
+          onMouseLeave={() => setVisible(false)}>
+          <NavLink className='navbar-link' style={{ textDecoration: 'none', color: 'black' }}
+            to='/'>Learn</NavLink></div>
+
+        <div className='navbar-right'>
+          <button className='navbar-trynow'>TRY NOW</button>
+          <div><img height='30' width='30' src={profile} /></div>
+        </div>
       </div>
-      <div className='navbar-logo'>
-        Cawfee <img height='100' width='100' src={cawfeeCrow}/>
-      </div>
-      <div>
-        Get Started
-      </div>
+
+      {visibility &&
+        <div>
+          <div className='navbar-underline'></div>
+          <div className='navbar-dropdown'
+            onMouseLeave={() => setVisible(false)}
+            onMouseEnter={() => {
+              setVisible(true)
+              setCoffeeVisible(true)
+            }}
+          >
+            {coffeeVisible && <div className='navbar-dropdown-coffee'>
+              <span><NavLink className='navbar-dropdown-coffee-selection' style={{ textDecoration: 'none', color: 'black' }} to='/'>All Coffee</NavLink></span>
+              <span><NavLink className='navbar-dropdown-coffee-selection' style={{ textDecoration: 'none', color: 'black' }} to='/'>Single Origin</NavLink></span>
+              <span><NavLink className='navbar-dropdown-coffee-selection' style={{ textDecoration: 'none', color: 'black' }} to='/'>Blends</NavLink></span>
+              <span><NavLink className='navbar-dropdown-coffee-selection' style={{ textDecoration: 'none', color: 'black' }} to='/'>Light Roast</NavLink></span>
+              <span><NavLink className='navbar-dropdown-coffee-selection' style={{ textDecoration: 'none', color: 'black' }} to='/'>Medium Roast</NavLink></span>
+              <span><NavLink className='navbar-dropdown-coffee-selection' style={{ textDecoration: 'none', color: 'black' }} to='/'>Dark Roast</NavLink></span>
+            </div>}
+          </div>
+        </div>}
     </nav>
   )
   // return (
