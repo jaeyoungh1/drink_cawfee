a93b8b282c9aff54f6745162bd92924afd3c3c20
Jae Hwang <jaeyoung1997@gmail.com>
2022-11-07 15:41:26 -0800

    troubleshooting adding to cart

--- react-app/src/components/singleCoffee/singleCoffee.js
+++ react-app/src/components/singleCoffee/singleCoffee.js
@@ -50,7 +50,7 @@ export default function SingleCoffee() {
         if (quantity < 1) {
             return;
         }
-        console.log("CURRENT CARTED COFFEE", cartedCoffee)
+        console.log("CURRENT CARTED COFFEE", cartedCoffee, +coffeeId)
         console.log("DOES THE CART INCLUDE CURRENT COFFEE",cartedCoffee?.includes(+coffeeId))
         if (cartedCoffee && cartedCoffee.includes(+coffeeId)) {
             let currentCart = cartArr.filter(obj => +obj.coffee_id === +coffeeId)
